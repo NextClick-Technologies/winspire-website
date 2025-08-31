3180c8ce3738b585532f2837482658c1d6cdf5cc
nmcyber <nmcybersolutions@gmail.com>
2025-08-31 13:14:38 +0300

    fix: added umami monitoring script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import { GeistMono } from 'geist/font/mono'
-import type { Metadata } from 'next'
-import { Plus_Jakarta_Sans } from 'next/font/google'
-import Navbar from '@/components/navbar'
-import FooterSection from '@/components/sections/footer'
-import ScrollToTop from '../components/scroll-to-top'
-import SmoothScroll from '../components/smooth-scroll'
-import './globals.css'
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { GeistMono } from "geist/font/mono";
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Navbar from "@/components/navbar";
+import FooterSection from "@/components/sections/footer";
+import ScrollToTop from "../components/scroll-to-top";
+import SmoothScroll from "../components/smooth-scroll";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
-  title: 'Winspire Consultancy',
-  description: 'Winspire Consultancy',
-}
+  title: "Winspire Consultancy",
+  description: "Winspire Consultancy",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <head>
         <style>{`
 html {
@@ -34,6 +35,12 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+        <Script
+          async
+          defer
+          src='https://umami.nmcyber.com/script.js'
+          data-website-id='042c63bb-33a2-4e27-8ddd-a2a1c448bc59'
+        />
       </head>
       <body className={`${plusJakartaSans.className}`}>
         <SmoothScroll />
@@ -45,5 +52,5 @@ html {
       <Analytics />
       <SpeedInsights />
     </html>
-  )
+  );
 }
